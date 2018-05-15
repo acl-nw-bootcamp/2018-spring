2cf0347a5ad80b814bf2dbab7ed86296fab0cc93
acl-nw-bootcamp <mark@alchemycodelab.com>
2018-05-14 20:35:10 -0700

    Class 08 Demo

---
+++ lectures/08-sql-intro-and-postgres/demos/in-class/class.js
@@ -0,0 +1,7 @@
+const classArray = [student1, student2, student3, student4];
+
+const classObject = {};
+classObject[student1.id] = student1;
+classObject[student2.id] = student2;
+classObject[student3.id] = student3;
+classObject[student4.id] = student4;
---
+++ lectures/08-sql-intro-and-postgres/demos/in-class/students.js
@@ -0,0 +1,31 @@
+const student1 = {
+  id: "1234-abcd-0000",
+  name: "John Doe",
+  address: "1234 S. Main Street",
+  city: "Portland",
+  email: "john@school.edu"
+}
+
+const student2 = {
+  id: "5678-efgh-0002",
+  name: "Jane Citizen",
+  address: "555 NW East Street",
+  city: "Hillsboro",
+  email: "jane@alchemycodelab.com"
+}
+
+const student3 = {
+  id: "2233-zzyy-1234",
+  name: "Jake Person",
+  address: "12332 SW Wherever",
+  city: "Beaverton",
+  email: "jake@somewhere.com"
+}
+
+const student4 = {
+  id: "7676-lmno-9876",
+  name: "Julie Student",
+  address: "321 NE Northpole Drive",
+  city: "Portland",
+  email: "julie567@hotmail.com"
+}
